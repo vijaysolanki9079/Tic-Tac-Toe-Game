57c1d830d17c1a715bbd71dc79b7906a55d43113
Vijay Solanki <vijaysolanki9079@gmail.com>
2024-09-15 13:25:11 +0530

    commit project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Board from "./components/Board";
+import "./style.css";
+
+function App() {
+  return (
+    <div>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
